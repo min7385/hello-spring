b3ee5730b95ee151e359ed9cfb3e4225117b7011
min7385 <rkd7385@naver.com>
2026-09-08 23:09:53 +0900

    feat:스프링 통합 테스트

---
+++ src/test/java/com/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,53 @@
+package com.hello.hello_spring.service;
+
+import com.hello.hello_spring.domain.Member;
+import com.hello.hello_spring.repository.MemberRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() throws Exception {
+        // Given
+        Member member = new Member();
+        member.setName("hello");
+
+        // When
+        Long saveId = memberService.join(member);
+
+        // Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // Given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // When
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 이름입니다.");
+    }
+}
